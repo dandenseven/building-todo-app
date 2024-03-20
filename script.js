21722dcdd1e9ca2bd2daf1c8d1642392de7bc8ec
Dane knight <knight1seven@gmail.com>
2024-03-19 21:00:27 -0400

    created addEventListner for closeTaskFormBtn, cancelBtn, discardBtn and taskForm

--- script.js
+++ script.js
@@ -18,3 +18,21 @@ let currentTask = {};
 openTaskFormBtn.addEventListener("click", () => {
     taskForm.classList.toggle("hidden");
 });
+
+closeTaskFormBtn.addEventListener("click", () => {
+    confirmCloseDialog.showModal();
+});
+
+
+cancelBtn.addEventListener("click", () => {
+    confirmCloseDialog.close();
+});
+
+discardBtn.addEventListener("click", () => {
+    confirmCloseDialog.close();
+    taskForm.classList.toggle("hidden");
+})
+
+taskForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+})
